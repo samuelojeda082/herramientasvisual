67a6504283a1d21d597ad2292d8d62eaee83a8eb
samuelojeda082 <74929322+samuelojeda082@users.noreply.github.com>
2021-05-11 07:29:07 +0200

    Actualizar versión

--- graph.js
+++ graph.js
@@ -1,10 +1,10 @@
 
-//Read the data
+// Cargamos los datos.
 d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main/dataset_act_grup.csv").then(function (data) {
 
   console.log(data);
 
-  // set the dimensions and margins of the graph
+  // definimos los márgenes, la anchura y altura
   var margin = {
     top: 40,
     right: 230,
@@ -14,7 +14,7 @@ d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main
     width = 600 - margin.left - margin.right,
     height = 620 - margin.top - margin.bottom;
 
-  // append the svg object to the body of the page
+  // Añadimos la gráfica con la etiqueta svg
   var svg = d3.select("#grafica")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -22,11 +22,8 @@ d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  // ---------------------------//
-  //       AXIS  AND SCALE      //
-  // ---------------------------//
 
-  // Add X axis
+  // Añade el eje x
   var x = d3.scaleLinear()
     .domain([-1.5, 1.5])
     .range([0, width]);
@@ -34,14 +31,14 @@ d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x).ticks(3));
 
-  // Add X axis label:
+  // Añade la etiqueta al eje x, en este caso Variación de población respecto al año anterior
   svg.append("text")
     .attr("text-anchor", "end")
     .attr("x", width)
     .attr("y", height + 50)
     .text("Variación de población respecto al año anterior");
 
-  // Add X axis 0
+  // Añade la linea vertical que coincide con el valor 0 del eje x
   svg.append("line")
     .attr("x1", 155)
     .attr("y1", 0)
@@ -52,14 +49,14 @@ d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main
     .style("stroke", "gray")
     .style("fill", "none");
 
-  // Add Y axis
+  // Añade el eje y
   var y = d3.scaleLinear()
     .domain([0, 40000])
     .range([height, 0]);
   svg.append("g")
     .call(d3.axisLeft(y));
 
-  // Add Y axis label:
+  // Añade la etiqueta al eje y, en este caso PIB per capita
   svg.append("text")
     .attr("text-anchor", "end")
     .attr("x", 0)
@@ -67,12 +64,12 @@ d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main
     .text("PIB per capita")
     .attr("text-anchor", "start")
 
-  // Add a scale for bubble size
+  // Define la escala del tamaño de las burbujas
   var z = d3.scaleSqrt()
     .domain([10000, 10000000])
     .range([2, 30]);
 
-  // Add a scale for bubble color
+  // Define los colores que tomarán cada una de las comunidades autónomas
   var myColor = d3.scaleOrdinal()
     .domain([...new Set(data.map(x => x.comuni))])
     .range([
@@ -95,12 +92,7 @@ d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main
       "#ff99ff"
     ]);
 
-
-  // ---------------------------//
-  //      TOOLTIP               //
-  // ---------------------------//
-
-  // -1- Create a tooltip div that is hidden by default:
+  // Se crea el tooltip
   var tooltip = d3.select("#grafica")
     .append("div")
     .style("opacity", 0)
@@ -110,7 +102,9 @@ d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main
     .style("padding", "10px")
     .style("color", "white")
 
-  // -2- Create 3 functions to show / update (when mouse move but stay on same circle) / hide the tooltip
+  // Definimos las funciones de los eventos que tendrá el tooltip. 
+  // Las funciones se definen aquí, pero se usarán en los eventos definidos abajo del script
+  // 1. Definimos los parámetros para la muestra del tooltip, cuando el ratón esté encima de una provincia
   var showTooltip = function (d) {
     tooltip
       .transition()
@@ -121,11 +115,13 @@ d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main
       .style("left", (d3.mouse(this)[0] + 30) + "px")
       .style("top", (d3.mouse(this)[1] + 30) + "px")
   }
+  // 2. Definimos la función cuando el puntero del ratón está encima del tooltip
   var moveTooltip = function (d) {
     tooltip
       .style("left", (d3.mouse(this)[0] + 30) + "px")
       .style("top", (d3.mouse(this)[1] + 30) + "px")
   }
+  // 3. Definimos la función que tomará el evento cuando el ratón salga de la provincia
   var hideTooltip = function (d) {
     tooltip
       .transition()
@@ -134,29 +130,23 @@ d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main
   }
 
 
-  // ---------------------------//
-  //       HIGHLIGHT GROUP      //
-  // ---------------------------//
-
-  // What to do when one group is hovered
+  // Definimos la función que cambia la opacidad de las burbujas cuando se está produciendo un evento. 
+  // Es decir, cuando el puntero está encima de una burbuja
   var highlight = function (d) {
-    // reduce opacity of all groups
-    d3.selectAll(".bubbles").style("opacity", .05)
-    // expect the one that is hovered
+    // reduce la opacidad de todas las burbujas
+    d3.selectAll(".bubbles").style("opacity", .02)
+    // Cuando el ratón está encima de una comunidad en la leyenda, las burbujas asociadas a esa comunidad
+    // se ponen con opacidad a 1.
     d3.selectAll("." + d).style("opacity", 1)
   }
 
-  // And when it is not hovered anymore
+  // Definimos la función cuando no hay ningún evento sobre la leyenda, es decir, todas las burbujas con opacidad 1
   var noHighlight = function (d) {
     d3.selectAll(".bubbles").style("opacity", 1)
   }
 
 
-  // ---------------------------//
-  //       CIRCLES              //
-  // ---------------------------//
-
-  // Add dots
+  // Definimos las burbujas que tendrá la gráfica
   svg.append('g')
     .selectAll("dot")
     .data(data)
@@ -177,18 +167,13 @@ d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main
     .style("fill", function (d) {
       return myColor(d.comuni);
     })
-    // -3- Trigger the functions for hover
+    // Definimos los eventos y las funciones que se ejecutarán cuando se produzca su evento correspondiente
     .on("mouseover", showTooltip)
     .on("mousemove", moveTooltip)
     .on("mouseleave", hideTooltip)
 
 
-
-  // ---------------------------//
-  //       LEGEND              //
-  // ---------------------------//
-
-  // Add legend: circles
+  // Definición de la leyenda
   var valuesToShow = [1000000, 4000000, 10000000]
   var xCircle = 420
   var xLabel = 470
@@ -206,8 +191,6 @@ d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main
     })
     .style("fill", "none")
     .attr("stroke", "black")
-
-  // Add legend: segments
   svg
     .selectAll("legend")
     .data(valuesToShow)
@@ -226,7 +209,7 @@ d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main
     .attr('stroke', 'black')
     .style('stroke-dasharray', ('2,2'))
 
-  // Add legend: labels
+  // Añade las etiquetas a la leyenda
   svg
     .selectAll("legend")
     .data(valuesToShow)
@@ -242,15 +225,15 @@ d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main
     .style("font-size", 10)
     .attr('alignment-baseline', 'middle')
 
-  // Legend title
+  // Título de la leyenda
   svg.append("text")
     .attr('x', xCircle)
     .attr("y", height + 50)
     .text("Población (millones)")
     .attr("text-anchor", "middle")
 
-  // Add one dot in the legend for each name.
-  var size = 20
+  // Añade los puntos a cada etiqueta de la leyenda
+  var size = 20 // Tamaño del punto
   var allgroups = [...new Set(data.map(x => x.comuni))]
   svg.selectAll("myrect")
     .data(allgroups)
@@ -259,15 +242,16 @@ d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main
     .attr("cx", 390)
     .attr("cy", function (d, i) {
       return 10 + i * (size + 5)
-    }) // 100 is where the first dot appears. 25 is the distance between dots
+    })
     .attr("r", 7)
     .style("fill", function (d) {
       return myColor(d)
     })
+    // Definición de los eventos sobre la leyenda
     .on("mouseover", highlight)
     .on("mouseleave", noHighlight)
 
-  // Add labels beside legend dots
+  // Agrega las etiquetas al lado de los puntos
   svg.selectAll("mylabels")
     .data(allgroups)
     .enter()
@@ -275,7 +259,7 @@ d3.csv("https://raw.githubusercontent.com/samuelojeda082/herramientasvisual/main
     .attr("x", 390 + size * .8)
     .attr("y", function (d, i) {
       return i * (size + 5) + (size / 2)
-    }) // 100 is where the first dot appears. 25 is the distance between dots
+    }) 
     .style("fill", function (d) {
       return myColor(d)
     })
